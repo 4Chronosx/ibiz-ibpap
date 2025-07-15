0b10eb815054a3a9ebae213464835dab8b6736f6
Jae <jadejaena@gmail.com>
2025-07-15 11:34:42 +0800

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import LandingPage from './pages/opening_pages/LandingPage';
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
   );
 }
 
 export default App
+
+
+
+
